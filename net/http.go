dbc67681522eab22e99ea72351b65415663ee383
Marianne Feng <feng.marianne@gmail.com>
2017-12-06 21:33:19 -0800

    went over networking utility package. Kind of a mess. Tried to remove dependency on go-til's logger but alas, needed logging. Still need to review //TODO with jessi

---
+++ net/http.go
@@ -0,0 +1,19 @@
+package net
+
+import (
+	"github.com/meatballhat/negroni-logrus"
+	"github.com/sirupsen/logrus"
+	"github.com/urfave/negroni"
+	"net/http"
+)
+
+// todo: get rid of negroni
+
+// InitNegroni is a helper function for starting up http servers. it will create a new Negroni instance,
+// use attach a logrus instance with a json formatter, and register the log instance with the appName
+// it will also attach a handler, so really all you have to call is Run() on the returned instance
+func InitNegroni(appName string, handler http.Handler) (n *negroni.Negroni){
+	n = negroni.New(negroni.NewRecovery(), negroni.NewStatic(http.Dir("public")))
+	n.UseHandler(handler)
+	return
+}
---
+++ net/models.go
@@ -0,0 +1,9 @@
+package net
+
+//HttpError is the format that errors should be returned from all REST api's - see usage in admin.go
+type ApiHttpError struct {
+	ErrorDescription string
+	Error  string
+	Status int
+}
+
